150eca5eaf33ba6f0edac8b409ee51bf9820f2aa
jose suzuki <jcbrandine@gmail.comgit config --global user.email jcbrandine@gmail.com>
2026-04-11 14:20:01 -0300

    teste

--- src/Domain/Enteties/Client.java
+++ src/Domain/Enteties/Client.java
@@ -1,4 +1,21 @@
 package Domain.Enteties;
 
 public class Client {
+    private String name;
+    private String cpf;
+
+
+    public Client(String name, String cpf){
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome do cliente é obrigatório");
+        }
+        if (cpf == null || cpf.trim().isEmpty()) {
+            throw new IllegalArgumentException("CPF é Obrigatório");
+        }
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    public String getName() {return name;}
+    public String getCpf() {return cpf;}
 }
