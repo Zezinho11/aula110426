d47f8a8077fd3243ec702e717645da8cc72b494b
jose suzuki <jcbrandine@gmail.comgit config --global user.email jcbrandine@gmail.com>
2026-04-11 21:31:07 -0300

    product in the making.

--- src/Domain/Enteties/Product.java
+++ src/Domain/Enteties/Product.java
@@ -5,8 +5,20 @@ public class Product {
     private double preco;
     private int estoque;
 
-    public Product(){
-        
+    public Product(String name, double preco,int estoque){
+    if (name == null || name.trim().isEmpty()){
+        throw new IllegalArgumentException("nome é obrigatório.");
+    }
+    if (preco <= 0) {
+        throw new IllegalArgumentException("Preço deve ser maior que R$0,00.");
+
+    }
+
+    this.name = name;
+    this.preco = preco;
+    this.estoque = estoque;
+
+
     }
 
 
