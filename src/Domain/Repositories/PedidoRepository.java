fb9b1f454e4225d52c3e1952da19c035bef81952
jose suzuki <jcbrandine@gmail.comgit config --global user.email jcbrandine@gmail.com>
2026-04-11 23:19:01 -0300

    first interface added.

--- src/Domain/Repositories/PedidoRepository.java
+++ src/Domain/Repositories/PedidoRepository.java
@@ -1,4 +1,11 @@
 package Domain.Repositories;
 
+import Domain.Enteties.Pedido;
+
+import java.util.List;
+
 public interface PedidoRepository {
+    void salvar(Pedido pedido);
+    List<Pedido> listar();
+    int proximoNumero();
 }
