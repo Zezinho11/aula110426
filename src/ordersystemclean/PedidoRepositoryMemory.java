b36f7c15a00090b81722978dfd6db8d10e0111b6
jose suzuki <jcbrandine@gmail.comgit config --global user.email jcbrandine@gmail.com>
2026-04-11 23:33:58 -0300

    Memory repository added

---
+++ src/ordersystemclean/PedidoRepositoryMemory.java
@@ -0,0 +1,20 @@
+package ordersystemclean;
+
+import Domain.Enteties.Pedido;
+import Domain.Repositories.PedidoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PedidoRepositoryMemory implements PedidoRepository{
+    private List<Pedido> pedidos;
+
+    {
+        pedidos = new ArrayList<;
+    };
+    public void salvar(Pedido pedido) {pedidos.add(pedido);}
+    public List<Pedido> Listar() {return pedidos;}
+    public int proximoNumero() { return pedidos.size() + 1;}
+
+}
+
