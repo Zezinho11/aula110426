4318558623967fd1bf6badf64560798b7663391f
jose suzuki <jcbrandine@gmail.comgit config --global user.email jcbrandine@gmail.com>
2026-04-11 21:23:07 -0300

    ado ado ado td mundo q usa java é viado

--- src/Domain/Enteties/Product.java
+++ src/Domain/Enteties/Product.java
@@ -1,4 +1,21 @@
 package Domain.Enteties;
 
 public class Product {
+    private String name;
+    private double preco;
+    private int estoque;
+
+    public Product(){
+        
+    }
+
+
+
+
+
+
+
 }
+
+
+
