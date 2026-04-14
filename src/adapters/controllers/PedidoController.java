d66e8ba4452c35199c09d77b00b00e0ec629a439
José Suzuki <jcbrandine@gmail.com>
2026-04-14 13:04:25 -0300

    Controller declared.

--- src/adapters/controllers/PedidoController.java
+++ src/adapters/controllers/PedidoController.java
@@ -1,7 +1,31 @@
 package adapters.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import Application.DTO.NewPedidoRequest;
+import Application.usercases.ListPedidosUseCase;
+import Application.usercases.NewPedidoUseCase;
+import Domain.Enteties.Pedido;
+import Domain.Enteties.Product;
+
+
 public class PedidoController {
-    public void 
+    private NewPedidoUseCase newPedidoUseCase;
+    private ListPedidosUseCase listPedidosUseCase;
+    private Scanner scanner = new Scanner(System.in);
+
+     public PedidoController(NewPedidoUseCase newPedidoUseCase, ListPedidosUseCase listPedidosUseCase ){
+         this.newPedidoUseCase = newPedidoUseCase;
+         this.listPedidosUseCase = listPedidosUseCase;
+     }
+
+     public void iniciarPedido(){
+         int opcao;
+
+
+     }
+
 
 
 }
