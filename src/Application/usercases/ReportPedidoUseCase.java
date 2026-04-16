588177c023b87aedb0ce63ba2f02e477c7da91f1
José Suzuki <jcbrandine@gmail.com>
2026-04-16 15:08:54 -0300

    ReportPedidoUseCase done. ReportPedidoResponse added

--- src/Application/usercases/ReportPedidoUseCase.java
+++ src/Application/usercases/ReportPedidoUseCase.java
@@ -1,10 +1,46 @@
 package Application.usercases;
 import Domain.Enteties.Pedido;
 import Domain.Repositories.PedidoRepository;
+import java.util.List;
 
 public class ReportPedidoUseCase {
 
-    
+        private final PedidoRepository pedidoRepository;
+
+        public ReportPedidoUseCase(PedidoRepository pedidoRepository) {
+            this.pedidoRepository = pedidoRepository;
+        }
+
+        public ReportPedidoUseCase executar(){
+            List<Pedido> pedidos = pedidoRepository.listar();
+
+            int totalPedidos = 0;
+            int totalcancelados = 0;
+            double totalReceita = 0;
+            double totalReceitaCancelada = 0;
+
+            for (Pedido pedido : pedidos) {
+            totalPedidos++;
+            if (pedido.getStories().equals("Cancelado")) {
+                totalcancelados++;
+                totalReceitaCancelada += pedido.getTotal();
+            } else {
+                totalReceita += pedido.getTotal();
+            }
+
+            return new ReportPedidoResponse(
+              totalPedidos,
+              totalcancelados,
+              totalReceita,
+              totalReceitaCancelada,
+            );
+
+
+
+            }
+
+
+        }
 
 
 
