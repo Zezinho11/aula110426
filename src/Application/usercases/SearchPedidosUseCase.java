04727c18d12d451d7a0ef4e36dba0ebe3dbdc5e9
José Suzuki <jcbrandine@gmail.com>
2026-04-16 14:51:58 -0300

    SearchPedidosUseCase done.

--- src/Application/usercases/SearchPedidosUseCase.java
+++ src/Application/usercases/SearchPedidosUseCase.java
@@ -1,4 +1,31 @@
 package Application.usercases;
 
+import Domain.Enteties.Pedido;
+import Domain.Repositories.PedidoRepository;
+
+import java.util.Optional;
+
 public class SearchPedidosUseCase {
+
+    private PedidoRepository pedidoRepository;
+
+    public SearchPedidosUseCase(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido executar(int numero){
+        if (numero < 0) {
+            throw new IllegalArgumentException("Numero de pedido inválido.");
+        }
+
+        Optional<Pedido> pedido = pedidoRepository.buscarPorNumero(numero);
+
+        if (pedido.isEmpty()) {
+            throw new IllegalArgumentException("Pedido número " + numero + "não encontrado.");
+        }
+
+        return pedido.get();
+
+    }
+
 }
